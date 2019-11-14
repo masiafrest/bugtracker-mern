ad49d0a0643779e89f5da83dd851f01eb5198f75
Julio Zeng <masiafrest@gmail.com>
2019-11-14 17:16:18 -0500

    1

--- app.js
+++ app.js
@@ -6,20 +6,16 @@ const path = require("path");
 
 // settings
 app.set("port", process.env.PORT || 4000);
-
-//const env = process.env.NODE_ENV;
-/* if (env === "production") {
-  app.use("/", express.static(`${__dirname}/frontend/build/`));
-} */
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
+const env = process.env.NODE_ENV;
+console.log(env);
+if (env !== "production") {
+  app.use("/", express.static(`${__dirname}/frontend/public/`));
+}
 // middlewares
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-//app.use(express.static(path.join(__dirname, "frontend", "build")));
 
 // routes
 app.use("/api/cards", require("./routes/cards"));
--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 require("dotenv").config();
 
-//const app = require("./app");
+const app = require("./app");
 require("./database");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const morgan = require("morgan");
-const path = require("path");
 
-// settings
-app.set("port", process.env.PORT || 4000);
-
-//const env = process.env.NODE_ENV;
-/* if (env === "production") {
-  app.use("/", express.static(`${__dirname}/frontend/build/`));
-} */
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-// middlewares
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
-//app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-// routes
-app.use("/api/cards", require("./routes/cards"));
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
-
-/* async function main() {
+async function main() {
   await app.listen(app.get("port"));
   console.log("server on port", app.get("port"));
-} */
-app.listen(app.get("port"));
-console.log("server on port", app.get("port"));
-/* main(); */
+}
+
+main();
